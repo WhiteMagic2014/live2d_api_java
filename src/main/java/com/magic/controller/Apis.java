f7b3ef2ae496d962c2b0b307ede689039e978351
magic chen <418379149@qq.com>
2019-03-24 18:31:35 +0800

    修改手滑产生的问题

--- src/main/java/com/magic/controller/Apis.java
+++ src/main/java/com/magic/controller/Apis.java
@@ -1,4 +1,4 @@
-package com.magic.apis;
+package com.magic.controller;
 
 import java.io.IOException;
 import java.util.Random;
@@ -16,16 +16,16 @@ import com.magic.util.ModelList;
 import com.magic.util.ModelTextures;
 
 @RestController
-public class apis {
+public class Apis {
 
 	@RequestMapping("/get")
-	public JSONObject get(@RequestParam("id") String id ,HttpServletResponse response) throws IOException {
+	public JSONObject get(@RequestParam("id") String id, HttpServletResponse response) throws IOException {
 //		System.out.println("get");
 		String[] ids = id.split("-");
 		int modelId = Integer.valueOf(ids[0]);
 		int modelTexturesId = 0;
 		if (ids.length > 1) {
-			modelTexturesId = Integer.valueOf(ids[1])==0 ? 1 : Integer.valueOf(ids[1]);
+			modelTexturesId = Integer.valueOf(ids[1]) == 0 ? 1 : Integer.valueOf(ids[1]);
 		}
 		if (modelId <= 0 || modelTexturesId <= 0) {
 			return new JSONObject();
@@ -91,7 +91,7 @@ public class apis {
 	@RequestMapping("/switch")
 	public JSONObject switchRole(@RequestParam("id") Integer id) throws IOException {
 //		System.out.println("switch");
-		
+
 		if (id == null) {
 			id = 0;
 		}
@@ -108,7 +108,7 @@ public class apis {
 		JSONObject result = new JSONObject();
 		result.put("model", new JSONObject().fluentPut("id", modelId)
 //				.fluentPut("name", models.get(id))
-				.fluentPut("message", messages.get(modelId-1)));
+				.fluentPut("message", messages.get(modelId - 1)));
 
 		System.out.println(result);
 		return result;
@@ -117,7 +117,7 @@ public class apis {
 	@RequestMapping("/switch_textures")
 	public JSONObject switchTextures(@RequestParam("id") String id) throws IOException {
 //		System.out.println("switch_textures");
-		
+
 		String[] ids = id.split("-");
 		int modelId = Integer.valueOf(ids[0]);
 		int modelTexturesId = 0;
@@ -145,7 +145,7 @@ public class apis {
 			newid = 1;
 		}
 		if ("".equals(realModelName)) {
-			realModelName = texturesArray.getString(newid-1);
+			realModelName = texturesArray.getString(newid - 1);
 		}
 
 		JSONObject result = new JSONObject();
@@ -156,28 +156,26 @@ public class apis {
 	@RequestMapping("/rand")
 	public JSONObject randRole(@RequestParam("id") Integer id) throws IOException {
 //		System.out.println("rand");
-		
+
 		JSONObject modelList = ModelList.getList();
 		JSONArray models = modelList.getJSONArray("models");
 		JSONArray messages = modelList.getJSONArray("messages");
-		
+
 		Random rand = new Random();
-		
+
 		int newNum = rand.nextInt(models.size()) + 1;
 		while (newNum == id) {
 			newNum = rand.nextInt(models.size()) + 1;
 		}
 		JSONObject result = new JSONObject();
-		result.put("model", new JSONObject()
-							.fluentPut("id", newNum)
-							.fluentPut("message", messages.get(newNum-1)));
+		result.put("model", new JSONObject().fluentPut("id", newNum).fluentPut("message", messages.get(newNum - 1)));
 		return result;
 	}
 
 	@RequestMapping("/rand_textures")
 	public JSONObject randTextures(@RequestParam("id") String id) throws IOException {
 //		System.out.println("rand_textures");
-		
+
 		String[] ids = id.split("-");
 		int modelId = Integer.valueOf(ids[0]);
 		int modelTexturesId = 0;
@@ -190,26 +188,26 @@ public class apis {
 		Object modelName = ModelList.idToName(modelId);
 		String realModelName = "";
 		JSONArray texturesArray = null;
-		
+
 		if (modelName instanceof JSONArray) {
 			texturesArray = ((JSONArray) modelName);
 		} else {
 			realModelName = modelName.toString();
 			texturesArray = ModelTextures.getList(realModelName);
 		}
-		
+
 		Random rand = new Random();
 		int newid = rand.nextInt(texturesArray.size()) + 1;
 		while (newid == modelTexturesId) {
 			newid = rand.nextInt(texturesArray.size()) + 1;
 		}
 		if ("".equals(realModelName)) {
-			realModelName = texturesArray.getString(newid-1);
+			realModelName = texturesArray.getString(newid - 1);
 		}
-		
+
 		JSONObject result = new JSONObject();
 		result.put("textures", new JSONObject().fluentPut("id", newid).fluentPut("name", realModelName));
 		return result;
 	}
-	
+
 }
