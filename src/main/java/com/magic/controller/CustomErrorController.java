5cfa7323cddddb891f555d5cb18accc9bb3179f3
magic chen <418379149@qq.com>
2020-09-14 17:53:49 +0800

    -- 新增本机错误页面

---
+++ src/main/java/com/magic/controller/CustomErrorController.java
@@ -0,0 +1,106 @@
+package com.magic.controller;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 
+ * @ClassName: CustomErrorController
+ * @Description: 自定义的错误处理controller 来代替spring 默认的Whitelabel Error Page ||
+ *               默认的json返回格式
+ * @author: magic chen
+ * @date: Sep 14, 2020 4:52:11 PM
+ * @Copyright
+ */
+@Controller
+public class CustomErrorController extends BasicErrorController {
+
+	public CustomErrorController(ServerProperties serverProperties) {
+		super(new DefaultErrorAttributes(), serverProperties.getError());
+	}
+
+	/**
+	 * 覆盖默认的Json响应
+	 */
+	@Override
+	public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+		Map<String, Object> body = getErrorAttributes(request, isIncludeStackTrace(request, MediaType.ALL));
+		HttpStatus status = getStatus(request);
+
+		Iterator it = body.keySet().iterator();
+		while (it.hasNext()) {
+			String key = it.next().toString();
+		}
+
+		if (500 <= status.value() && status.value() < 600) {
+			body.put("code", "500");
+			body.put("message", "服务器内部错误");
+		} else if (status.value() == 403) {
+			body.put("code", "403");
+			body.put("message", "无权限");
+		} else if (status.value() == 404) {
+			body.put("code", "404");
+			body.put("message", "接口不存在");
+		} else if (status.value() == 400) {
+			body.put("code", "400");
+			body.put("message", "缺少所需参数");
+		} else {
+			body.put("code", "未知错误");
+		}
+		body.remove("trace");
+		body.remove("error");
+
+		return new ResponseEntity<>(body, status);
+	}
+
+	/**
+	 * 覆盖默认的HTML响应
+	 */
+	@Override
+	public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
+
+		HttpStatus status = getStatus(request);
+		response.setStatus(status.value());
+
+		Map<String, Object> errMap = getErrorAttributes(request, isIncludeStackTrace(request, MediaType.TEXT_HTML));
+
+		// 内部遍历看看有些什么东西
+		Iterator it = errMap.keySet().iterator();
+		while (it.hasNext()) {
+			String key = it.next().toString();
+		}
+
+		if (500 <= status.value() && status.value() < 600) {
+			errMap.put("message", "服务器内部错误");
+		} else if (status.value() == 403) {
+			errMap.put("message", "无权限");
+		} else if (status.value() == 404) {
+			errMap.put("message", "该页面不见了");
+		} else if (status.value() == 400) {
+			errMap.put("message", "缺少所需参数");
+		}
+
+		errMap.put("server", "你正在使用 live2d 后台接口服务");
+		errMap.remove("trace");
+		errMap.remove("error");
+
+		ModelAndView mv = new ModelAndView("ErrorPage");
+		mv.setStatus(status);
+		mv.addAllObjects(errMap);
+
+		return mv;
+	}
+
+}
---
+++ src/main/resources/static/l2d/autoload.js
@@ -0,0 +1,35 @@
+const live2d_path = "l2d/";
+//const live2d_path = "./";
+
+$("<link>").attr({href: live2d_path + "waifu.css", rel: "stylesheet", type: "text/css"}).appendTo("head");
+//waifu.css的绝对路径
+
+$.ajax({
+	url: live2d_path + "live2d.min.js",
+	dataType: "script",
+	cache: true,
+	async: false
+});
+//live2d.min.js的绝对路径
+
+$.ajax({
+	url: live2d_path + "waifu-tips.js",
+	dataType: "script",
+	cache: true,
+	async: false
+});
+//waifu-tips.js的绝对路径
+
+//初始化看板娘，会自动加载指定目录下的waifu-tips.json
+$(window).on("load", function() {
+//    原作者php
+//    initWidget(live2d_path + "waifu-tips.json", "https://live2d.fghrsh.net/api");
+//    本机
+    initWidget(live2d_path + "waifu-tips.json", "http://localhost:8080/live2d");
+//    我的java版
+//    initWidget(live2d_path + "waifu-tips.json", "https://server.whitemagic2014.com/live2d");
+});
+
+//initWidget第一个参数为waifu-tips.json的绝对路径
+//第二个参数为api地址（无需修改）
+//api后端可自行搭建，参考https://github.com/fghrsh/live2d_api
